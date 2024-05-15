25b44f77e264c89e5f0e947644f6f02ee896fbdc
DartingEditing <me.upocta26@gmail.com>
2024-05-16 02:21:42 +0800

    UPDATE command youtube

--- www/main.js
+++ www/main.js
@@ -1,13 +1,46 @@
 $(document).ready(function () {
-    
+
     $('.text').textillate({
         loop: true,
         sync: true,
-        in:{
+        in: {
             effect: "bounceIn",
         },
-        out:{
+        out: {
             effect: "bounceOut",
         },
-    })
+    });
+
+    // siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 640,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+    });
+
+    // siri message
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
+
+    // micbutton click
+    $("#MicBtn").click(function () {
+        eel.playAssistantSound()
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);        
+        eel.allCommands()()
+    });
 });
