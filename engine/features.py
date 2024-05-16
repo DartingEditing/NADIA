89a691b2a1d215ad87311fbbad5b96d53e2cb029
DartingEditing <me.upocta26@gmail.com>
2024-05-16 11:04:28 +0800

    Update database SW and open url DONE

--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,7 @@
 import os
 import re
+import sqlite3
+import webbrowser
 from playsound import playsound
 import eel
 from engine.command import speak
@@ -7,6 +9,9 @@ from engine.config import ASSISTANT_NAME
 
 # play assistant sound function
 import pywhatkit as kit
+
+con = sqlite3.connect("nadia.db")
+cursor = con.cursor()
 
 @eel.expose
 def playAssistantSound():
@@ -17,12 +22,36 @@ def openCommand(query):
     query = query.replace(ASSISTANT_NAME, "")
     query = query.replace("open", "")
     query.lower()
+    app_name = query.strip()
 
-    if query!="":
-        speak("Opening "+query)
-        os.system('start '+query)
-    else:
-        speak("not found")
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
 
 def PlayYoutube(query):
     search_term = extract_yt_term(query)
